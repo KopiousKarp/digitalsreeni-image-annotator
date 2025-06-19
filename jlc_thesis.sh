b094b8be526a7daf9786df7efd36b90ad3174dc5
J. Cristiano <jcristia@udel.edu>
2025-06-19 13:31:40 -0400

    Enhance jlc_thesis.sh to detect GPU architecture and streamline Docker image build process; add apple.dockerfile for Apple Silicon support

--- jlc_thesis.sh
+++ jlc_thesis.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+
+#####################################################################################
 # detect system gpu architecture before building
 # Initialize GPU_TYPE variable
 GPU_TYPE="UNKNOWN"
@@ -14,27 +17,55 @@ elif command -v lspci &> /dev/null && lspci | grep -i "amd" | grep -i "vga\|3d\|
 # Check for Apple Silicon (M1, M2, etc.)
 elif [ "$(uname)" == "Darwin" ] && sysctl -n machdep.cpu.brand_string 2>/dev/null | grep -q "Apple M"; then
     echo "Apple Silicon detected"
-    GPU_TYPE="m1"
+    GPU_TYPE="apple"
 fi
 
 echo "Detected GPU architecture: $GPU_TYPE"
 export GPU_TYPE
-
 if [ "$GPU_TYPE" == "UNKNOWN" ]; then
     echo "No supported GPU detected. Exiting."
     exit 1
 fi
-
-# Proceed with the build process
-echo "Proceeding with build for $GPU_TYPE architecture..."
-
-# Build the Docker image with the appropriate tag
-echo "Building Docker image with tag: sreeni_$GPU_TYPE"
-if docker build -t "sreeni_$GPU_TYPE" -f "./env/$GPU_TYPE.dockerfile" ./env; then
-    echo "Docker image built successfully with tag: sreeni_$GPU_TYPE"
+###########################################################################################
+# Check if Docker image already exists
+echo "Checking if Docker image with tag sreeni_$GPU_TYPE already exists..."
+if docker images "sreeni_$GPU_TYPE" | grep -q "sreeni_$GPU_TYPE"; then
+    echo "Docker image with tag sreeni_$GPU_TYPE already exists. Skipping build."
 else
-    echo "Failed to build Docker image. Exiting."
-    exit 1
+    echo "Docker image with tag sreeni_$GPU_TYPE does not exist. Building new image."
+    # Build the Docker image with the appropriate tag
+    echo "Building Docker image with tag: sreeni_$GPU_TYPE"
+    if docker build -t "sreeni_$GPU_TYPE" -f "./env/$GPU_TYPE.dockerfile" ./env; then
+        echo "Docker image built successfully with tag: sreeni_$GPU_TYPE"
+
+    else
+        echo "Failed to build Docker image. Exiting."
+        exit 1
+    fi
 fi
 
 
+
+
+
+##############################################################################################
+echo "running environment"
+xhost +
+docker run -it \
+      --net=host \
+      --cap-add=SYS_PTRACE \
+      --security-opt seccomp=unconfined \
+      --device=/dev/kfd \
+      --device=/dev/dri \
+      --group-add video \
+      --ipc=host \
+      --shm-size 8G \
+      -v ./:/work \
+      -v /media:/media \
+      -v $HOME/.Xauthority:/root/.Xauthority \
+      -v /tmp/.X11-unix:/tmp/.X11-unix \
+      -e DISPLAY=$DISPLAY \
+      sreeni_$GPU_TYPE bash
+
+
+echo "docker command executed successfully."
